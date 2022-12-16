994049372aeedc5139280d8b2439c8a7964fbbb5
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-12-16 14:43:11 -0500

    updated webhook again 2

--- routes/stripeWebhook.js
+++ routes/stripeWebhook.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const { handleStripeWebhook } = require('../controllers/stripeWebhookController')
+const bodyParser = require("body-parser")
 
 const router = express.Router()
 
 // overall route: /api/webhook/stripe-webhook 
-router.post("/stripe-webhook", handleStripeWebhook)
+router.post("/stripe-webhook", bodyParser.raw({type:"application/json"}), handleStripeWebhook)
 
 module.exports = router 
