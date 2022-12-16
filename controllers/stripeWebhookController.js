132ede6a499056a1624a4057d13462deea1e1bc5
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-12-16 17:58:43 -0500

    altered webhook

--- controllers/stripeWebhookController.js
+++ controllers/stripeWebhookController.js
@@ -3,12 +3,12 @@ const Stripe = require("stripe")
 const stripe = Stripe(process.env.STRIPE_PRIVATE_KEY)
 
 let handleStripeWebhook = async (req, res) => {
+  
+  // verify that the event comes from stripe 
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET
   console.log('webhook called')
-
   let stripeEvent
   if (endpointSecret) {
-    // Get the signature sent by Stripe
     const signature = req.headers['stripe-signature'];
     try {
       stripeEvent = stripe.webhooks.constructEvent(
@@ -16,22 +16,24 @@ let handleStripeWebhook = async (req, res) => {
         signature,
         endpointSecret
       );
+      console.log('stripeEvent: ', stripeEvent)
     } catch (err) {
       console.log(`⚠️  Webhook signature verification failed.`, err.message);
       res.send({err: err.message});
     }
   }
-  if (stripeEvent.type === "payment_intent.succeeded"){
-    const paymentIntent = stripeEvent.data.object
-    console.log('full stripe paymentIntent obj after success: ', paymentIntent)
-    console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`)
-    // update db with an order here 
+
+  // if (stripeEvent.type === "payment_intent.succeeded"){
+  //   const paymentIntent = stripeEvent.data.object
+  //   console.log('full stripe paymentIntent obj after success: ', paymentIntent)
+  //   console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`)
+  //   // update db with an order here 
     
-  } else if (stripeEvent.type === "payment_intent.payment_failed"){
-    const paymentIntent = stripeEvent.data.object
-    console.log('full stripe paymentIntent obj after failure: ', paymentIntent)
-    console.log(`PaymentIntent for ${paymentIntent.amount} failed!`)
-  }
+  // } else if (stripeEvent.type === "payment_intent.payment_failed"){
+  //   const paymentIntent = stripeEvent.data.object
+  //   console.log('full stripe paymentIntent obj after failure: ', paymentIntent)
+  //   console.log(`PaymentIntent for ${paymentIntent.amount} failed!`)
+  // }
 }
 
 module.exports = { handleStripeWebhook };
--- routes/stripeWebhook.js
+++ routes/stripeWebhook.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const { handleStripeWebhook } = require('../controllers/stripeWebhookController')
-const bodyParser = require("body-parser")
 
 const router = express.Router()
 
 // overall route: /api/webhook/stripe-webhook 
-router.post("/stripe-webhook", bodyParser.raw({type:"application/json"}), handleStripeWebhook)
+router.post("/stripe-webhook", handleStripeWebhook)
 
 module.exports = router 
