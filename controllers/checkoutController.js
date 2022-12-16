32f3958c12e5c6751e2ab95a932033297b471716
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-12-16 11:14:01 -0500

    webhook for checkout version1

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -82,9 +82,33 @@ const handleGetDetailsThenCheckout = async (req, res) => {
 }
 
 let handleStripeWebhook = async (req, res) => {
-  // validate that req came from Stripe 
-  // const sig = req.headers["stripe-signature"]; 
-  // const endpointSecret = 
+  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET
+  let stripeEvent
+  if (endpointSecret) {
+    // Get the signature sent by Stripe
+    const signature = request.headers['stripe-signature'];
+    try {
+      stripeEvent = stripe.webhooks.constructEvent(
+        request.body,
+        signature,
+        endpointSecret
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`, err.message);
+      return response.sendStatus(400);
+    }
+  }
+  if (stripeEvent.type === "payment_intent.succeeded"){
+    const paymentIntent = stripeEvent.data.object
+    console.log('full stripe paymentIntent obj after success: ', paymentIntent)
+    console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`)
+    // update db with an order here 
+    
+  } else if (stripeEvent.type === "payment_intent.payment_failed"){
+    const paymentIntent = stripeEvent.data.object
+    console.log('full stripe paymentIntent obj after failure: ', paymentIntent)
+    console.log(`PaymentIntent for ${paymentIntent.amount} failed!`)
+  }
   console.log('handleStripeWebhook called')
 }
 
