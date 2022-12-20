da33d8a6ef665d32569020a1034d10812216c5b1
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-12-19 20:27:06 -0500

    first attempt at creating an order object in db

--- controllers/stripeWebhookController.js
+++ controllers/stripeWebhookController.js
@@ -19,7 +19,8 @@ let handleStripeWebhook = async (req, res) => {
         endpointSecret
       );
       // this works 
-      res.send({stripeEvent: stripeEvent})
+      // res.send({stripeEvent: stripeEvent})
+
       console.log('stripeEvent: ', stripeEvent)
     } catch (err) {
       console.log(`⚠️  Webhook signature verification failed.`, err.message);
@@ -28,14 +29,51 @@ let handleStripeWebhook = async (req, res) => {
   }
 
   if (stripeEvent.type === "payment_intent.succeeded"){
-    const paymentIntent = stripeEvent.data.object
+    // const paymentIntent = stripeEvent.data.object
     // update db with an order here 
+    let parsedCart = JSON.parse(req.body.data.object.metadata.cart)
+
     let orderObj = {}
-    // orderObj["user"] = 
+    orderObj["user"] = req.body.data.object.metadata.user
+    orderObj["orderItems"] = []
+
+    let totalCost = 0
+
+    for (let i = 0; i < parsedCart.cartArray.length; i++){
+      let currentItem = parsedCart.cartArray[i]
+      let newObj = {}
+      newObj["quantity"] = currentItem.quantity
+      newObj["id"] = currentItem.id
+      orderObj["orderItems"].push(newObj)
+      
+      // add to totalCost
+      let currentItemTotalCost = currentItem.price * currentItem.quantity
+      totalCost += currentItemTotalCost
+    }
+
+    orderObj["totalCost"] = totalCost
     
+    let shippingAddress = {}
+    shippingAddress["city"] = req.body.data.object.customerDetails.address.city
+    shippingAddress["county"] = req.body.data.object.customerDetails.address.county
+    shippingAddress["line1"] = req.body.data.object.customerDetails.address.line1
+    shippingAddress["line2"] = req.body.data.object.customerDetails.address.line2
+    shippingAddress["postal_code"] = req.body.data.object.customerDetails.address.postal_code
+    shippingAddress["state"] = req.body.data.object.customerDetails.address.state
+    orderObj["shippingAddress"] = shippingAddress
+
+    // use orderObj to create a new instance of the order class in the db 
+    try {
+      const newOrder = await Order.create(orderObj)
+      res.status(200).send(newOrder)
+    } catch (err) {
+      res.status(400).json({ mssg: err.message })
+    }
+
   } else if (stripeEvent.type === "payment_intent.payment_failed"){
-    const paymentIntent = stripeEvent.data.object
+    // const paymentIntent = stripeEvent.data.object
     // send back failure message 
+    res.send({error: "The payment attempt failed."})
   }
 }
 
