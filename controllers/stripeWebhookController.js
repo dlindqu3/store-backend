c700a19069b2c5d8e4218055e752a95313677385
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-12-21 13:52:35 -0500

    testing stripeEnv

--- controllers/stripeWebhookController.js
+++ controllers/stripeWebhookController.js
@@ -20,7 +20,9 @@ let handleStripeWebhook = async (req, res) => {
         signature,
         endpointSecret
       )
+      console.log("webhook verified")
     } catch (err) {
+      console.log("webhook error message: ", err.message)
       res.status(400).send({err: err.message});
     }
   }
@@ -44,63 +46,68 @@ let handleStripeWebhook = async (req, res) => {
 
   // SHOULD THIS BE "payment_intent.succeeded"? payment_intent is listed elsewhere on the req and incoming res 
   if (stripeEvent.type === "checkout.session.completed"){
-    const paymentIntent = stripeEvent.data.object
+    console.log("checkout.session.completed")
     // update db with an order here 
-    let parsedCart = JSON.parse(req.body.data.object.metadata.cart)
+    // let parsedCart = JSON.parse(req.body.data.object.metadata.cart)
 
-    orderObj["user"] = req.body.data.object.metadata.user
+    // orderObj["user"] = req.body.data.object.metadata.user
 
-    // ADD CUSTOMER ID HERE 
-    // search for customer by email 
-    let customerObj = await stripe.customers.list({
-      email: req.body.data.object.customer_details.email
-    })
-    orderObj["customer"] = customerObj.data[0].id
+    // // ADD CUSTOMER ID HERE 
+    // // search for customer by email 
+    // let customerObj = await stripe.customers.list({
+    //   email: req.body.data.object.customer_details.email
+    // })
 
-    orderObj["orderItems"] = []
+    // console.log('customerObj: ', customerObj)
+    // orderObj["customer"] = customerObj.data[0].id
 
-    let totalCost = 0
+    // orderObj["orderItems"] = []
 
-    for (let i = 0; i < parsedCart.cartArray.length; i++){
-      let currentItem = parsedCart.cartArray[i]
-      let newObj = {}
-      newObj["quantity"] = currentItem.quantity
-      newObj["id"] = currentItem.id
-      orderObj["orderItems"].push(newObj)
+    // let totalCost = 0
+
+    // for (let i = 0; i < parsedCart.cartArray.length; i++){
+    //   let currentItem = parsedCart.cartArray[i]
+    //   let newObj = {}
+    //   newObj["quantity"] = currentItem.quantity
+    //   newObj["id"] = currentItem.id
+    //   orderObj["orderItems"].push(newObj)
       
-      // add to totalCost
-      let currentItemTotalCost = currentItem.price * currentItem.quantity
-      totalCost += currentItemTotalCost
-    }
+    //   // add to totalCost
+    //   let currentItemTotalCost = currentItem.price * currentItem.quantity
+    //   totalCost += currentItemTotalCost
+    // }
 
-    orderObj["totalCost"] = totalCost
+    // orderObj["totalCost"] = totalCost
     
-    let shippingAddress = {}
-    shippingAddress["city"] = req.body.data.object.customerDetails.address.city
-    shippingAddress["county"] = req.body.data.object.customerDetails.address.county
-    shippingAddress["line1"] = req.body.data.object.customerDetails.address.line1
-    shippingAddress["line2"] = req.body.data.object.customerDetails.address.line2
-    shippingAddress["postal_code"] = req.body.data.object.customerDetails.address.postal_code
-    shippingAddress["state"] = req.body.data.object.customerDetails.address.state
-    orderObj["shippingAddress"] = shippingAddress
+    // let shippingAddress = {}
+    // shippingAddress["city"] = req.body.data.object.customerDetails.address.city
+    // shippingAddress["county"] = req.body.data.object.customerDetails.address.county
+    // shippingAddress["line1"] = req.body.data.object.customerDetails.address.line1
+    // shippingAddress["line2"] = req.body.data.object.customerDetails.address.line2
+    // shippingAddress["postal_code"] = req.body.data.object.customerDetails.address.postal_code
+    // shippingAddress["state"] = req.body.data.object.customerDetails.address.state
+    // orderObj["shippingAddress"] = shippingAddress
  
 
-    // use orderObj to create a new instance of the order class in the db 
-    let dbOrder
-    try {
-      const newOrder = new Order(orderObj)
-      dbOrder = await newOrder.save()
-      // res.status(200).send(savedOrder)
-    } catch (err) {
-      res.status(400).send({ mssg: err.message })
-    }
+    // console.log('orderObj: ', orderObj)
+
+    // // use orderObj to create a new instance of the order class in the db 
+    // let dbOrder
+    // try {
+    //   const newOrder = new Order(orderObj)
+    //   dbOrder = await newOrder.save()
+    //   console.log('dbOrder: ', dbOrder)
+    //   // res.status(200).send(savedOrder)
+    // } catch (err) {
+    //   res.status(400).send({ mssg: err.message })
+    // }
   } else if (stripeEvent.type !== "checkout.session.completed"){
-    // const paymentIntent = stripeEvent.data.object
     // send back failure message 
     res.send({error: "The payment attempt failed."})
   }
 
-  res.send({stripeEv: stripeEvent})
+  console.log('stripeEv: ', stripeEvent)
+  res.send({stripeEv: stripeEvent}).end()
 }
 
 module.exports = { handleStripeWebhook };
