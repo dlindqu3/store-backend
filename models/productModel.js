60c8ecf0e69d8ea0d8ec6e716c2ea78f165197cb
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-12-25 15:35:38 -0500

    updated product model to include timestamp

--- models/productModel.js
+++ models/productModel.js
@@ -1,42 +1,45 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
-  name: {
-    type: String, 
-    required: true, 
-    unique: true
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      default: 0,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    quantityInStock: {
+      type: Number,
+      required: false,
+      default: 750,
+    },
   },
-  image: {
-    type: String, 
-    required: true
-  },
-  description: {
-    type: String, 
-    required: true, 
-    default: 0
-  },
-  brand: {
-    type: String, 
-    required: true
-  },
-  category: {
-    type: String, 
-    required: true
-  },
-  price: {
-    type: Number, 
-    required: true, 
-    default: 0
-  },
-  quantityInStock: {
-    type: Number, 
-    required: false,
-    default: 750
+  {
+    timestamps: true,
   }
-})
+);
 
-module.exports = mongoose.model('Product', productSchema)
-
-
+module.exports = mongoose.model("Product", productSchema);
